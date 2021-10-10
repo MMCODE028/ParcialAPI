58cfb33c71ac1e63ae632920ed55dadcc17c7603
MMCODE028 <62720864+MMCODE028@users.noreply.github.com>
2021-10-10 15:44:28 -0500

    first commit

---
+++ defaultclientdb.py
@@ -0,0 +1,9 @@
+client = [
+    {
+        "id": 57269,
+        "nombre": "Santiago",
+        "apellido": "Ospina Ojeda",
+        "direccion": "parque heredia",
+        "codigo_postal": "123446"
+    },
+]
---
+++ defaultpackages.py
@@ -0,0 +1,6 @@
+packages = [{
+    "id": 1,
+    "nombre": "Espada diamante",
+    "precio": 7000,
+    "trayectoria": 1
+}]
